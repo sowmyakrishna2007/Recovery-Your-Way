03d9a99ff397321ad993e3ea88c5b6c99b958790
sowmyakrishna2007 <89750530+sowmyakrishna2007@users.noreply.github.com>
2025-02-09 21:24:02 -0500

    Update addPost.js

--- addPost.js
+++ addPost.js
@@ -1,30 +1,26 @@
 import axios from 'axios';
 import { getAuth } from 'firebase/auth';
 import { url } from './urls';
-import { Alert } from 'react-native'; // Import Alert
+import { Alert } from 'react-native'; 
 
 export default storePost = async (user, post) => {
   try {
     const token = await user.getIdToken(true);
     const userId = user.uid;
-    // Generate a unique ID for the post
     const postId = new Date().toISOString();
 
-    // Post data to store
     const postData = {
       username: user.displayName,
       userId: userId,
       post: post,
-      timestamp: postId, // Use timestamp as unique ID
+      timestamp: postId, 
     };
 
-    // Store post in user's personal node
     await axios.post(`${url}/users/${userId}/posts.json?auth=${token}`, postData);
 
-    // Optionally store post in publicPosts node if it's public
     await axios.post(`${url}/posts.json?auth=${token}`, postData);
 
   } catch (error) {
-    Alert.alert('Error', `Error storing post`); // Use Alert for error
+    Alert.alert('Error', `Error storing post`); 
   }
 };
